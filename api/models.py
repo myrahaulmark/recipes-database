4765f0392ebdc1af3942024505658fd9ee089c81
Myra Haulmark <haulmark@uark.edu>
2024-10-11 12:20:20 -0500

    redo

--- api/models.py
+++ api/models.py
@@ -2,211 +2,232 @@
 
 class Category:
     
-    def __init__(self, category_id: int, category_name: str):
-        self.category_id = category_id
-        self.category_name = category_name
+    def __init__(self, CategoryID: int, CategoryName: str):
+        self.CategoryID = CategoryID
+        self.CategoryName = CategoryName
         
     def __repr__(self):
-        return f'<Category {self.category_id} - {self.category_name}>'
+        return f'<Category {self.CategoryID} - {self.CategoryName}>'
     
     def to_dict(self):
         return {
-            'category_id': self.category_id,
-            'category_name': self.category_name
+            'CategoryID': self.CategoryID,
+            'CategoryName': self.CategoryName
         }
 
 def create_category_from_dict(data: dict) -> Category:
-    return Category(data['category_id'], data['category_name'])
+    return Category(data['CategoryID'], data['CategoryName'])
+
 
 class User:
     
-    def __init__(self, user_id: int, first_name: str, last_name: str, email: str, join_date: str):
-        self.user_id = user_id
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.join_date = join_date
+    def __init__(self, UserID: int, FirstName: str, LastName: str, Email: str, JoinDate: str):
+        self.UserID = UserID
+        self.FirstName = FirstName
+        self.LastName = LastName
+        self.Email = Email
+        self.JoinDate = JoinDate
         
     def __repr__(self):
-        return f'<User {self.user_id} - {self.first_name} {self.last_name}>'
+        return f'<User {self.UserID} - {self.FirstName} {self.LastName}>'
     
     def to_dict(self):
         return {
-            'user_id': self.user_id,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'email': self.email,
-            'join_date': self.join_date
+            'UserID': self.UserID,
+            'FirstName': self.FirstName,
+            'LastName': self.LastName,
+            'Email': self.Email,
+            'JoinDate': self.JoinDate
         }
 
 def create_user_from_dict(data: dict) -> User:
-    return User(data['user_id'], data['first_name'], data['last_name'], data['email'], data['join_date'])
+    return User(data['UserID'], data['FirstName'], data['LastName'], data['Email'], data['JoinDate'])
+
 
 class Recipe:
     
-    def __init__(self, recipe_id: int, title: str, description: str, cooking_time: int, servings: int, number_of_steps: int, submitted_date: str, number_of_ingredients: int, image_url: str):
-        self.recipe_id = recipe_id
-        self.title = title
-        self.description = description
-        self.cooking_time = cooking_time
-        self.servings = servings
-        self.number_of_steps = number_of_steps
-        self.submitted_date = submitted_date
-        self.number_of_ingredients = number_of_ingredients
-        self.image_url = image_url
+    def __init__(self, RecipeID: int, Title: str, Description: str, CookingTime: int, Servings: int, NumberOfSteps: int, Instructions: str, SubmittedDate: str, NumberOfIngredients: int, ImageURL: str):
+        self.RecipeID = RecipeID
+        self.Title = Title
+        self.Description = Description
+        self.CookingTime = CookingTime
+        self.Servings = Servings
+        self.NumberOfSteps = NumberOfSteps
+        self.Instructions = Instructions
+        self.SubmittedDate = SubmittedDate
+        self.NumberOfIngredients = NumberOfIngredients
+        self.ImageURL = ImageURL
         
     def __repr__(self):
-        return f'<Recipe {self.recipe_id} - {self.title}>'
+        return f'<Recipe {self.RecipeID} - {self.Title}>'
     
     def to_dict(self):
         return {
-            'recipe_id': self.recipe_id,
-            'title': self.title,
-            'description': self.description,
-            'cooking_time': self.cooking_time,
-            'servings': self.servings,
-            'number_of_steps': self.number_of_steps,
-            'submitted_date': self.submitted_date,
-            'number_of_ingredients': self.number_of_ingredients,
-            'image_url': self.image_url
+            'RecipeID': self.RecipeID,
+            'Title': self.Title,
+            'Description': self.Description,
+            'CookingTime': self.CookingTime,
+            'Servings': self.Servings,
+            'NumberOfSteps': self.NumberOfSteps,
+            'Instructions': self.Instructions,
+            'SubmittedDate': self.SubmittedDate,
+            'NumberOfIngredients': self.NumberOfIngredients,
+            'ImageURL': self.ImageURL
         }
 
 def create_recipe_from_dict(data: dict) -> Recipe:
-    return Recipe(data['recipe_id'], data['title'], data['description'], data['cooking_time'], data['servings'], data['number_of_steps'], data['submitted_date'], data['number_of_ingredients'], data['image_url'])
+    return Recipe(data['RecipeID'], data['Title'], data['Description'], data['CookingTime'], data['Servings'], data['NumberOfSteps'], data['Instructions'], data['SubmittedDate'], data['NumberOfIngredients'], data['ImageURL'])
 
 class Ingredient:
     
-    def __init__(self, ingredients_id: int, ingredients: str, serving_size: int, calories: int, total_fat: int, saturated_fat: int, cholesterol: int, sodium: int, carbohydrate: int, fiber: int, sugars: int, protein: int, fat: int, caffeine: int, source: str, date: str, is_allergen: str, food_allergen_id: int, food: str, food_class: str, food_type: str, food_group: str, allergy_type: str):
-        self.ingredients_id = ingredients_id
-        self.ingredients = ingredients
-        self.serving_size = serving_size
-        self.calories = calories
-        self.total_fat = total_fat
-        self.saturated_fat = saturated_fat
-        self.cholesterol = cholesterol
-        self.sodium = sodium
-        self.carbohydrate = carbohydrate
-        self.fiber = fiber
-        self.sugars = sugars
-        self.protein = protein
-        self.fat = fat
-        self.caffeine = caffeine
-        self.source = source
-        self.date = date
-        self.is_allergen = is_allergen
-        self.food_allergen_id = food_allergen_id
-        self.food = food
-        self.food_class = food_class
-        self.food_type = food_type
-        self.food_group = food_group
-        self.allergy_type = allergy_type
+    def __init__(self, IngredientID: int, Ingredients: str, ServingSize: int, Calories: int, TotalFat: int, SaturatedFat: int, Cholesterol: int, Sodium: int, Carbohydrate: int, Fiber: int, Sugars: int, Protein: int, Fat: int, Caffeine: int, Source: str, Date: str, IsAllergen: str, Food_allergen_ID: int, Food: str, Food_Class: str, Food_Type: str, Food_Group: str, Allergy_Type: str):
+        self.IngredientID = IngredientID
+        self.Ingredients = Ingredients
+        self.ServingSize = ServingSize
+        self.Calories = Calories
+        self.TotalFat = TotalFat
+        self.SaturatedFat = SaturatedFat
+        self.Cholesterol = Cholesterol
+        self.Sodium = Sodium
+        self.Carbohydrate = Carbohydrate
+        self.Fiber = Fiber
+        self.Sugars = Sugars
+        self.Protein = Protein
+        self.Fat = Fat
+        self.Caffeine = Caffeine
+        self.Source = Source
+        self.Date = Date
+        self.IsAllergen = IsAllergen
+        self.Food_allergen_ID = Food_allergen_ID
+        self.Food = Food
+        self.Food_Class = Food_Class
+        self.Food_Type = Food_Type
+        self.Food_Group = Food_Group
+        self.Allergy_Type = Allergy_Type
         
     def __repr__(self):
-        return f'<Ingredient {self.ingredients_id} - {self.ingredients}>'
+        return f'<Ingredient {self.IngredientID} - {self.Ingredients}>'
     
     def to_dict(self):
         return {
-            'ingredients_id': self.ingredients_id,
-            'ingredients': self.ingredients,
-            'serving_size': self.serving_size,
-            'calories': self.calories,
-            'total_fat': self.total_fat,
-            'saturated_fat': self.saturated_fat,
-            'cholesterol': self.cholesterol,
-            'sodium': self.sodium,
-            'carbohydrate': self.carbohydrate,
-            'fiber': self.fiber,
-            'sugars': self.sugars,
-            'protein': self.protein,
-            'fat': self.fat,
-            'caffeine': self.caffeine,
-            'source': self.source,
-            'date': self.date,
-            'is_allergen': self.is_allergen,
-            'food_allergen_id': self.food_allergen_id,
-            'food': self.food,
-            'food_class': self.food_class,
-            'food_type': self.food_type,
-            'food_group': self.food_group,
-            'allergy_type': self.allergy_type
+            'IngredientID': self.IngredientID,
+            'Ingredients': self.Ingredients,
+            'ServingSize': self.ServingSize,
+            'Calories': self.Calories,
+            'TotalFat': self.TotalFat,
+            'SaturatedFat': self.SaturatedFat,
+            'Cholesterol': self.Cholesterol,
+            'Sodium': self.Sodium,
+            'Carbohydrate': self.Carbohydrate,
+            'Fiber': self.Fiber,
+            'Sugars': self.Sugars,
+            'Protein': self.Protein,
+            'Fat': self.Fat,
+            'Caffeine': self.Caffeine,
+            'Source': self.Source,
+            'Date': self.Date,
+            'IsAllergen': self.IsAllergen,
+            'Food_allergen_ID': self.Food_allergen_ID,
+            'Food': self.Food,
+            'Food_Class': self.Food_Class,
+            'Food_Type': self.Food_Type,
+            'Food_Group': self.Food_Group,
+            'Allergy_Type': self.Allergy_Type
         }
 
 def create_ingredient_from_dict(data: dict) -> Ingredient:
     return Ingredient(
-        data['ingredients_id'], data['ingredients'], data['serving_size'], data['calories'], 
-        data['total_fat'], data['saturated_fat'], data['cholesterol'], data['sodium'], 
-        data['carbohydrate'], data['fiber'], data['sugars'], data['protein'], data['fat'], 
-        data['caffeine'], data['source'], data['date'], data['is_allergen'], data['food_allergen_id'], 
-        data['food'], data['food_class'], data['food_type'], data['food_group'], data['allergy_type']
+        data['IngredientID'], data['Ingredients'], data['ServingSize'], data['Calories'], 
+        data['TotalFat'], data['SaturatedFat'], data['Cholesterol'], data['Sodium'], 
+        data['Carbohydrate'], data['Fiber'], data['Sugars'], data['Protein'], data['Fat'], 
+        data['Caffeine'], data['Source'], data['Date'], data['IsAllergen'], data['Food_allergen_ID'], 
+        data['Food'], data['Food_Class'], data['Food_Type'], data['Food_Group'], data['Allergy_Type']
     )
+
 
 class RecipeIngredientsFact:
     
-    def __init__(self, comp_id: int, recipe_id: int, ingredients_id: int, ingredients: str):
-        self.comp_id = comp_id
-        self.recipe_id = recipe_id
-        self.ingredients_id = ingredients_id
-        self.ingredients = ingredients
+    def __init__(self, CompID: int, RecipeID: int, IngredientID: int):
+        self.CompID = CompID
+        self.RecipeID = RecipeID
+        self.IngredientID = IngredientID
         
     def __repr__(self):
-        return f'<RecipeIngredientsFact {self.comp_id}>'
+        return f'<RecipeIngredientsFact {self.CompID}>'
     
     def to_dict(self):
         return {
-            'comp_id': self.comp_id,
-            'recipe_id': self.recipe_id,
-            'ingredients_id': self.ingredients_id,
-            'ingredients': self.ingredients
+            'CompID': self.CompID,
+            'RecipeID': self.RecipeID,
+            'IngredientID': self.IngredientID
         }
 
 def create_recipe_ingredients_fact_from_dict(data: dict) -> RecipeIngredientsFact:
-    return RecipeIngredientsFact(
-        data['comp_id'], data['recipe_id'], data['ingredients_id'], data['ingredients']
-    )
+    return RecipeIngredientsFact(data['CompID'], data['RecipeID'], data['IngredientID'])
 
 class Review:
     
-    def __init__(self, review_id: int, rating: int, review_text: str, review_date: str, user_id: int, recipe_id: int):
-        self.review_id = review_id
-        self.rating = rating
-        self.review_text = review_text
-        self.review_date = review_date
-        self.user_id = user_id
-        self.recipe_id = recipe_id
+    def __init__(self, ReviewID: int, Rating: int, ReviewText: str, ReviewDate: str, UserID: int, RecipeID: int):
+        self.ReviewID = ReviewID
+        self.Rating = Rating
+        self.ReviewText = ReviewText
+        self.ReviewDate = ReviewDate
+        self.UserID = UserID
+        self.RecipeID = RecipeID
         
     def __repr__(self):
-        return f'<Review {self.review_id} - {self.rating} stars>'
+        return f'<Review {self.ReviewID} - {self.Rating} stars>'
     
     def to_dict(self):
         return {
-            'review_id': self.review_id,
-            'rating': self.rating,
-            'review_text': self.review_text,
-            'review_date': self.review_date,
-            'user_id': self.user_id,
-            'recipe_id': self.recipe_id
+            'ReviewID': self.ReviewID,
+            'Rating': self.Rating,
+            'ReviewText': self.ReviewText,
+            'ReviewDate': self.ReviewDate,
+            'UserID': self.UserID,
+            'RecipeID': self.RecipeID
         }
 
 def create_review_from_dict(data: dict) -> Review:
-    return Review(
-        data['review_id'], data['rating'], data['review_text'], 
-        data['review_date'], data['user_id'], data['recipe_id']
-    )
+    return Review(data['ReviewID'], data['Rating'], data['ReviewText'], data['ReviewDate'], data['UserID'], data['RecipeID'])
+
 
 class RecipeCategoryFact:
     
-    def __init__(self, recipe_id: int, category_id: int):
-        self.recipe_id = recipe_id
-        self.category_id = category_id
+    def __init__(self, RecipeID: int, CategoryID: int):
+        self.RecipeID = RecipeID
+        self.CategoryID = CategoryID
         
     def __repr__(self):
-        return f'<RecipeCategoryFact RecipeID: {self.recipe_id}, CategoryID: {self.category_id}>'
+        return f'<RecipeCategoryFact RecipeID: {self.RecipeID}, CategoryID: {self.CategoryID}>'
     
     def to_dict(self):
         return {
-            'recipe_id': self.recipe_id,
-            'category_id': self.category_id
+            'RecipeID': self.RecipeID,
+            'CategoryID': self.CategoryID
         }
 
 def create_recipe_category_fact_from_dict(data: dict) -> RecipeCategoryFact:
-    return RecipeCategoryFact(data['recipe_id'], data['category_id'])
+    return RecipeCategoryFact(data['RecipeID'], data['CategoryID'])
+
+class Instruction:
+    
+    def __init__(self, InstructionID: int, StepCount: int, Instructions: str, RecipeID: int):
+        self.InstructionID = InstructionID
+        self.StepCount = StepCount
+        self.Instructions = Instructions
+        self.RecipeID = RecipeID
+        
+    def __repr__(self):
+        return f'<Instruction {self.InstructionID} - Step {self.StepCount}>'
+    
+    def to_dict(self):
+        return {
+            'InstructionID': self.InstructionID,
+            'StepCount': self.StepCount,
+            'Instructions': self.Instructions,
+            'RecipeID': self.RecipeID
+        }
+
+def create_instruction_from_dict(data: dict) -> Instruction:
+    return Instruction(data['InstructionID'], data['StepCount'], data['Instructions'], data['RecipeID'])
